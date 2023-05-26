9f25237e3b88e274a97df1746dee4a366ab072c4
Anna Lebid <annalez16@gmail.com>
2023-05-26 21:39:11 +0300

    create SearchBar

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const SearchBar = () => {
+  const router = useRouter();
+  const [location, setLocation] = useState("");
+  return (
+    <>
+      <input
+        type="text"
+        className="rounded text-lg mr-3 p-2 w-[450px]"
+        placeholder="State, city or town"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <button
+        className="bg-red-600 px-9 py-2 text-white rounded"
+        onClick={() => {
+          if (location === "banana") {
+            return;
+          }
+          router.push("/search");
+        }}
+      >
+        Let's Go
+      </button>
+    </>
+  );
+};
+
+export default SearchBar;
