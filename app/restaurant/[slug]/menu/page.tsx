c6a011b9495922b3b18d96c8feed1371273ac65e
Anna Lebid <annalez16@gmail.com>
2023-05-29 10:32:03 +0300

    add params to Menu and props to RestaurantNavBar

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -2,11 +2,11 @@ import Header from "../components/Header";
 import RestaurantNavBar from "../components/RestaurantNavBar";
 import RestaurantMenu from "../components/Menu";
 
-const Menu = () => {
+const Menu = ({ params }: { params: { slug: string } }) => {
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
+        <RestaurantNavBar slug={params.slug} />
         <RestaurantMenu />
       </div>
     </>
