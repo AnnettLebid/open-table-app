4aee3481e9be863bed753ef7037b7663c8eafd89
Anna Lebid <annalez16@gmail.com>
2023-06-05 08:29:06 +0300

    add user endpoint

---
+++ pages/api/auth/user.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const bearerToken = req.headers["authorization"] as string;
+  const token = bearerToken.split(" ")[1];
+
+  const payload = jwt.decode(token) as { email: string };
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: payload.email,
+    },
+    select: {
+      id: true,
+      first_name: true,
+      last_name: true,
+      email: true,
+      phone: true,
+      city: true,
+    },
+  });
+
+  return res.status(200).json({ user });
+}
