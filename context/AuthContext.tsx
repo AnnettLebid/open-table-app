1fd48d3e031d7f48c8a901831e38135c84e84205
Anna Lebid <annalez16@gmail.com>
2023-06-14 23:08:14 +0300

    update AuthContextProvider

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { useState, createContext, ReactNode, useContext } from "react";
+import {
+  useState,
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+} from "react";
+import axios from "axios";
+import { getCookie } from "cookies-next";
 
 interface User {
   id: number;
@@ -28,12 +36,59 @@ export const AuthenticationContext = createContext<AuthContextProps>({
   setAuthState: () => {},
 });
 
-export default function AuthContextProvider({ children }: { children: ReactNode }) {
+export default function AuthContextProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
   const [authState, setAuthState] = useState<AuthState>({
     loading: false,
     data: null,
     error: null,
   });
+
+  const fetchUser = async () => {
+    setAuthState({
+      data: null,
+      error: null,
+      loading: true,
+    });
+    try {
+      const jwt = getCookie("jwt");
+
+      if (!jwt) {
+        return setAuthState({
+          data: null,
+          error: null,
+          loading: false,
+        });
+      }
+
+      const response = await axios.get("http://localhost:3000/api/auth/user", {
+        headers: { Authorization: `Bearer ${jwt}` },
+      });
+      console.log("response", response);
+
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+
+      setAuthState({
+        data: response.data,
+        error: null,
+        loading: false,
+      });
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false,
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <AuthenticationContext.Provider value={{ ...authState, setAuthState }}>
       {children}
