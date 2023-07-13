8923ea7e8b71396e9bcaa39803fdcf07d89331f6
Anna Lebid <annalez16@gmail.com>
2023-07-13 22:08:31 +0300

    update Error component

--- app/restaurant/not-found.tsx
+++ app/restaurant/not-found.tsx
@@ -8,10 +8,9 @@ const Error = ({ error }: { error: Error }) => {
     <div className="h-screen bg-gray-200 flex flex-col">
       <Image src={ErrorImg} alt="error" className="w-56 mb-8 mx-auto" />
       <div className="bg-white px-9 py-14 shadow rounded justify-center text-center">
-        <h3 className="text-3xl font-bold ">Ooops, something went wrong</h3>
-        <p className="text-reg font-bold">
-          Ooops, we cannot find his restaurant
-        </p>
+        <h3 className="text-reg font-bold">
+          Ooops, we cannot find this restaurant
+        </h3>
       </div>
     </div>
   );
