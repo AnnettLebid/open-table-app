ea5086ce278fb51e48dbd4a6e40959ef4beda2da
Anna Lebid <annalez16@gmail.com>
2023-08-10 14:59:06 +0300

    create reservation endpoint

---
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { slug, day, time, partySize } = req.query as {
+    slug: string;
+    day: string;
+    time: string;
+    partySize: string;
+  };
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: { slug },
+  });
+
+  if (!restaurant) {
+    return res.status(400).json({ errorMessage: "Restaurant not found" });
+  }
+
+  if (
+    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+  ) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Restaurant is not opened at this time" });
+  }
+}
+//http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2023-07-28&time=14:00:00.000Z&partySize=2
