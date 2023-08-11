083a5704f4e659cd086736c4aac33d0d4772f499
Anna Lebid <annalez16@gmail.com>
2023-08-11 22:38:37 +0300

    add logic to determine what tables to book

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -39,5 +39,64 @@ export default async function handler(
     restaurant,
     res,
   });
+
+  if (!searchTimesWithTables) {
+    return res.status(400).json({ errorMessage: "Invalid data provided" });
+  }
+
+  const searchTimeWithTables = searchTimesWithTables.find((data) => {
+    return data.date.toISOString() === new Date(`${day}T${time}`).toISOString();
+  });
+
+  if (!searchTimeWithTables) {
+    return res
+      .status(400)
+      .json({ errorMessage: "No availability, cannot book" });
+  }
+
+  const tablesCount: {
+    2: number[];
+    4: number[];
+  } = {
+    2: [],
+    4: [],
+  };
+
+  searchTimeWithTables.tables.forEach((table) => {
+    if (table.seats === 2) {
+      tablesCount[2].push(table.id);
+    } else {
+      tablesCount[4].push(table.id);
+    }
+  });
+
+  const tablesToBook: number[] = [];
+  let seatsRemaining = parseInt(partySize);
+
+  while (seatsRemaining > 0) {
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining -= 4;
+      } else {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining -= 2;
+      }
+    } else {
+      if (tablesCount[2].length) {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining -= 2;
+      } else {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining -= 4;
+      }
+    }
+  }
+
+  return res.json({ tablesCount, tablesToBook });
 }
 //http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2023-08-28&time=14:00:00.000Z&partySize=2
